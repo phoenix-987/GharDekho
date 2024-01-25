d7270e7e4a1f9a3cb762ef2b5dfab17f1f94ac73
phoenix <naman.j@consultadd.com>
2024-01-25 11:43:25 -0500

    Implemented user registration feature.

--- GharDekho/settings.py
+++ GharDekho/settings.py
@@ -101,8 +101,6 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -143,3 +141,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authorization.User'
