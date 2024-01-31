bf3990d1ce9802b1e3b3bbf665ae6b5824515577
phoenix <naman.j@consultadd.com>
2024-01-31 13:15:07 -0500

    Redirected the endpoints towards the class-based views, i.e., APIViews.

--- properties/urls.py
+++ properties/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', list_all_properties, name='get_all_properties'),
-    path('<int:pk>/', list_property, name='get_property'),
-    path('add-property/', add_new_property, name='add_new_property'),
-    path('edit-property/<int:pk>/', edit_property, name='edit_property'),
-    path('delete-property/<int:pk>/', delete_property, name='delete_property'),
+    path('', PropertyListView.as_view(), name='get_all_properties'),
+    path('<int:pk>/', GetPropertyView.as_view(), name='get_property'),
+    path('add-property/', AddPropertyView.as_view(), name='add_new_property'),
+    path('edit-property/<int:pk>/', EditPropertyView.as_view(), name='edit_property'),
+    path('delete-property/<int:pk>/', DeletePropertyView.as_view(), name='delete_property'),
 ]
